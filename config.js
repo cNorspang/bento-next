c0e22cd7a4f2930960eadaee28ebfe584e800417
Casper Norspang <cbn@netic.dk>
2023-01-05 13:59:46 +0100

    Moving work

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: "John",
+  name: "Casper",
   openInNewTab: true,
   title: "Bento",
 
@@ -20,7 +20,7 @@ const CONFIG = {
   flashSeparator: false,
   
   // Theme
-  theme: "bento",
+  theme: "catppuccin-mocha",
   imageBackground: false,
   imageUrl: "background.jpg", // place in ./src/assets/images/ and provide the file name. Alternatively, provide a URL to an image. If the page is served over https, you may have issues loading images from insecure origins.
   themes: [   // List of installed themes, add your own themes in ./src/assets/css/themes and include it in the array below.
